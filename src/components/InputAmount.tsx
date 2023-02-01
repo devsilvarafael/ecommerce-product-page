12a233574044d6a1ec8cd0206eaa8f9808402e64
Rafael Silva <devsilva.rafael@gmail.com>
2023-02-01 16:15:51 -0300

    feat: Add amount verification with useReducer to previne negative values in input.

--- src/components/InputAmount.tsx
+++ src/components/InputAmount.tsx
@@ -1,13 +1,58 @@
 import { FaMinus as MinusIcon, FaPlus as PlusIcon } from "react-icons/fa"
+import { useReducer } from "react";
+
+enum InputTypes {
+    INCREASE = "INCREASE",
+    DECREASE = "DECREASE"
+}
+
+type InputAction = {
+    type: InputTypes;
+}
+
+type ProductAmount = {
+    amount: number;
+}
+
+function amountReducer (state: ProductAmount, action: InputAction) {
+    const { type } = action
+    if(type === InputTypes.INCREASE) {
+        return {
+            amount: state.amount + 1
+        }
+    }
+
+    if(type === InputTypes.DECREASE) {
+        if (state.amount === 0) {
+            return {
+                amount: 0
+            }
+        }
+        return {
+            amount: state.amount - 1
+        }
+    }
+
+    return {
+        amount: 0
+    }
+}
 
 export const InputAmount = () => {
+    const [state, dispatch] = useReducer(amountReducer, { amount: 0 })
+
     return (
         <div className={"flex flex-row bg-light-grayish-blue h-14 items-center rounded-xl w-3/5 justify-around"}>
-            <MinusIcon className={"text-orange cursor-pointer"} />
+            <MinusIcon className={"text-orange cursor-pointer"} onClick={() => dispatch({ type: InputTypes.DECREASE })}/>
 
-            <input type={"number"} className={"bg-transparent m-0 p-0 font-bold text-center max-w-[40px]"} defaultValue={0} />
+            <input
+                type={"number"}
+                className={"bg-transparent m-0 p-0 font-bold text-center max-w-[40px]"}
+                value={state.amount}
+                min={0}
+            />
 
-            <PlusIcon  className={"text-orange cursor-pointer"} />
+            <PlusIcon  className={"text-orange cursor-pointer"} onClick={() => dispatch({ type: InputTypes.INCREASE })}/>
         </div>
     )
 }
