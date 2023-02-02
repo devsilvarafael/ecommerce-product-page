71305b9c0bb66696b4da8076f0e9091588cb7e31
Rafael Silva <devsilva.rafael@gmail.com>
2023-02-01 21:56:01 -0300

    feat: ProductMainImage have a new width prop for different use in ProductPresentation and Modal.

--- src/components/ProductMainImage.tsx
+++ src/components/ProductMainImage.tsx
@@ -1,8 +1,20 @@
+import { Fragment } from "react";
+
 type ProductMainImageProps = {
     url: string;
     openOnScreen?: () => void;
+    width: "lg" | "md"
 }
 
-export const ProductMainImage = ({ url, openOnScreen }: ProductMainImageProps) => {
-    return <img src={url} alt={"Product Image"} className={"rounded-2xl"} onClick={openOnScreen}/>
+export const ProductMainImage = ({ url, openOnScreen, width }: ProductMainImageProps) => {
+    return (
+        <div className={"min-w-full md:min-w-[500px] min-h-[375px]"}>
+            <img
+                src={url}
+                alt={"Product Image"}
+                className={`${width === "lg" ? "max-w-xl" : "max-w-md"} md:rounded-2xl md:cursor-pointer`}
+                onClick={openOnScreen}
+            />
+        </div>
+    )
 }
