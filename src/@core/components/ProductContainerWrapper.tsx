18aa11f4f6929afb81703a577424019cbf982675
Rafael Silva <devsilva.rafael@gmail.com>
2023-02-02 18:47:00 -0300

    chore: add breakpoint.

--- src/@core/components/ProductContainerWrapper.tsx
+++ src/@core/components/ProductContainerWrapper.tsx
@@ -6,7 +6,7 @@ type ContainerWrapperProps = {
 
 export const ProductContainerWrapper = ({ children }: ContainerWrapperProps) => {
     return (
-        <div className={"w-full flex flex-col md:p-0 md:flex-row justify-evenly items-center md:mt-20"}>
+        <div className={"flex flex-col md:p-0 md:flex-row md:justify-evenly items-center md:mt-20"}>
             {children}
         </div>
     )
--- src/@core/components/ProductHero.tsx
+++ src/@core/components/ProductHero.tsx
@@ -10,11 +10,11 @@ export const ProductHero = ({ url, openOnScreen, width }: ProductMainImageProps)
     const { width: screenWidth } = useWindowSize()
 
     return (
-        <div className={"min-w-full md:min-w-[500px] min-h-[375px]"}>
+        <div className={"md:min-w-full md:min-w-[500px] md:min-h-[375px]"}>
             <img
                 src={url}
                 alt={"Product Image"}
-                className={`${width === "lg" ? "max-w-xl" : "max-w-md"} md:rounded-2xl md:cursor-pointer hover:shadow-md hover:transition`}
+                className={`${width === "lg" ? "max-w-xl" : "md:max-w-md"} md:rounded-2xl md:cursor-pointer hover:shadow-md hover:transition`}
                 onClick={screenWidth > 400 ? openOnScreen : null}
             />
         </div>
