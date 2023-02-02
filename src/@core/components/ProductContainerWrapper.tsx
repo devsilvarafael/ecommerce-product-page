684498e6acc2a50e3a92555a5da8f286966232eb
Rafael Silva <devsilva.rafael@gmail.com>
2023-02-01 23:24:35 -0300

    chore: Remove margin top issue.

--- src/@core/components/ProductContainerWrapper.tsx
+++ src/@core/components/ProductContainerWrapper.tsx
@@ -6,7 +6,7 @@ type ContainerWrapperProps = {
 
 export const ProductContainerWrapper = ({ children }: ContainerWrapperProps) => {
     return (
-        <div className={"w-full flex flex-col md:h-screen md:p-0 md:flex-row justify-evenly items-center md:-mt-16"}>
+        <div className={"w-full flex flex-col md:p-0 md:flex-row justify-evenly items-center md:mt-20"}>
             {children}
         </div>
     )
