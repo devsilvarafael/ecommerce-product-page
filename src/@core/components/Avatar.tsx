4e9e2a50465e87817aff60140f6adc18d6453567
Rafael Silva <devsilva.rafael@gmail.com>
2023-02-02 20:09:40 -0300

    feat: Add Avatar hover.

--- src/@core/components/Avatar.tsx
+++ src/@core/components/Avatar.tsx
@@ -2,7 +2,7 @@ import avatar from "/images/image-avatar.png"
 
 export const Avatar = () => {
     return (
-        <div className={"w-6 h-6 md:w-14 md:h-14 rounded-full"}>
+        <div className={"w-6 h-6 md:w-14 md:h-14 rounded-full hover:outline hover:outline-orange md:hover:outline-3 cursor-pointer"}>
             <img src={avatar} />
         </div>
     )
