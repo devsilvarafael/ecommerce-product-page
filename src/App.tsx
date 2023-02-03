880f9fe6e0517d15efa652ec18f325d9d0779e6f
Rafael Silva <devsilva.rafael@gmail.com>
2023-02-03 19:08:28 -0300

    chore: Provide ProductContextWrapper in App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import {Menu} from "./@core/components/Menu";
 import { ProductPresentation } from "./views/Product/ProductPresentation";
+import { ProductContextWrapper } from "@core/contexts/ProductContext";
 
 function App() {
     return (
-        <>
+        <ProductContextWrapper>
             <Menu />
             <ProductPresentation />
-        </>
+        </ProductContextWrapper>
     )
 }
 
