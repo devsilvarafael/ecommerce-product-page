e87b95bab0412d9c27d9bfc8f4a1526cd5cfffa8
Rafael Silva <devsilva.rafael@gmail.com>
2023-02-04 20:58:37 -0300

    chore: Fix semantic html issue.

--- src/@core/components/Product/ProductInfo.tsx
+++ src/@core/components/Product/ProductInfo.tsx
@@ -10,7 +10,7 @@ export const ProductInfo = ({ title, description }: ProductInfoProps) => {
         <section>
             <p className={"uppercase text-sm text-orange font-extrabold tracking-widest"}>Sneaker Company</p>
             <h1 className={"text-3xl md:text-5xl font-bold mt-4 mb-4 md:mb-12 max-w-xl"}>{title}</h1>
-            <p className={"mb-8 text-dark-grayish-blue max-w-lg"} role={"contentinfo"}>
+            <p className={"mb-8 text-dark-grayish-blue max-w-lg"}>
                 {description}
             </p>
         </section>
