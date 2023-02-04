6d921de92e0ea040b79098cc3444a65e4dc2eb4e
Rafael Silva <devsilva.rafael@gmail.com>
2023-02-04 18:15:59 -0300

    chore: Increase screen width validation for openOnScreen in ProductHero.

--- src/@core/components/Product/ProductHero.tsx
+++ src/@core/components/Product/ProductHero.tsx
@@ -15,7 +15,7 @@ export const ProductHero = ({ url, openOnScreen, width }: ProductMainImageProps)
                 src={url}
                 alt={"Product Image"}
                 className={`${width === "lg" ? "max-w-2xl" : "md:max-w-lg"} md:rounded-2xl md:cursor-pointer hover:shadow-md hover:transition`}
-                onClick={screenWidth > 400 ? openOnScreen : undefined}
+                onClick={screenWidth > 1000 ? openOnScreen : undefined}
             />
         </div>
     )
