713a6cc1279960d926b8a44bdde0b8465d31bccb
Rafael Silva <devsilva.rafael@gmail.com>
2023-02-05 05:28:39 -0300

    refactor: Fix mobile.

--- src/@core/components/Product/ProductInfo.tsx
+++ src/@core/components/Product/ProductInfo.tsx
@@ -9,8 +9,8 @@ export const ProductInfo = ({ title, description }: ProductInfoProps) => {
     return (
         <section className={"mt-10"}>
             <p className={"uppercase text-sm text-orange font-extrabold tracking-widest mb-4"}>Sneaker Company</p>
-            <h1 className={"text-3xl md:text-[43px] tracking-wide leading-tight font-extrabold max-w-xl mb-7"}>{title}</h1>
-            <p className={"text-dark-grayish-blue tracking-wide max-w-lg mb-7"}>
+            <h1 className={"text-3xl md:text-4xl tracking-wide leading-tight font-extrabold max-w-lg mb-7"}>{title}</h1>
+            <p className={"text-dark-grayish-blue tracking-wide max-w-xl mb-7"}>
                 {description}
             </p>
         </section>
